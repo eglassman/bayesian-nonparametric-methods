c448d9dc884d95142eaa73b1c731dd09bef028fb
eglassman <elena.glassman@gmail.com>
2016-04-07 14:04:10 -0400

    fixed gem code

--- gem_distribution.py
+++ gem_distribution.py
@@ -1,16 +1,17 @@
 import numpy
 
 alpha = 1
+number_of_sticks = 10
 
 betas = []
 
-for k in range(100):
+for k in range(number_of_sticks):
     beta_k = numpy.random.beta(1, alpha)
     betas.append(beta_k)
 
 print betas
 
-one_minus_betas = numpy.ones(100) - betas
+one_minus_betas = numpy.ones(number_of_sticks) - betas
 
 pis = []
 
@@ -19,7 +20,8 @@ for k,beta_k in enumerate(betas):
     if k == 0:
         pis.append(beta_k)
     elif k >= 1:
-        pi_k = beta_k * numpy.cumprod(one_minus_betas[0:k-1])[-1] #inefficient, but whatever
+        #print one_minus_betas[0:k-1]
+        pi_k = beta_k * numpy.cumprod(one_minus_betas[0:k])[-1] #inefficient, but whatever
         pis.append(pi_k)
     else:
         print 'something weird is happening; shouldnt get here'
