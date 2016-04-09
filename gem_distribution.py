bbc41e34b3302245471f814aac3813128198e1dd
eglassman <elena.glassman@gmail.com>
2016-04-09 16:54:37 -0400

    turned GEM distribution script into parameterized function

--- gem_distribution.py
+++ gem_distribution.py
@@ -1,28 +1,38 @@
+#GENERATE TRUNCATED GEM DISTRIBUTION
+
 import numpy
 
-alpha = 1
-number_of_sticks = 10
 
-betas = []
+def gem_distribution(alpha,number_of_clusters):
+    #alpha = 1
+    ##number_of_sticks = 10
 
-for k in range(number_of_sticks):
-    beta_k = numpy.random.beta(1, alpha)
-    betas.append(beta_k)
+    #since this is a stick-breaking construction of GEM
+    number_of_sticks = number_of_clusters 
 
-print betas
+    betas = []
 
-one_minus_betas = numpy.ones(number_of_sticks) - betas
+    for k in range(number_of_sticks):
+        beta_k = numpy.random.beta(1, alpha)
+        betas.append(beta_k)
 
-pis = []
+    print betas
 
-for k,beta_k in enumerate(betas):
-    print k,beta_k
-    if k == 0:
-        pis.append(beta_k)
-    elif k >= 1:
-        #print one_minus_betas[0:k-1]
-        pi_k = beta_k * numpy.cumprod(one_minus_betas[0:k])[-1] #inefficient, but whatever
-        pis.append(pi_k)
-    else:
-        print 'something weird is happening; shouldnt get here'
-print pis
+    one_minus_betas = numpy.ones(number_of_sticks) - betas
+
+    pis = []
+
+    for k,beta_k in enumerate(betas):
+        print k,beta_k
+        if k == 0:
+            pis.append(beta_k)
+        elif k >= 1:
+            #print one_minus_betas[0:k-1]
+            pi_k = beta_k * numpy.cumprod(one_minus_betas[0:k])[-1] #inefficient, but whatever
+            pis.append(pi_k)
+        else:
+            print 'something weird is happening; shouldnt get here'
+    return pis
+
+#test call
+gem_distribution(1,10)
