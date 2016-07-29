e60455271f0800e32ef7b65d6be81f3955ec012c
eglassman <elena.glassman@gmail.com>
2016-07-28 20:45:13 -0400

    adding rif on Tamaras sampler

--- ex6_sampler.py
+++ ex6_sampler.py
@@ -63,8 +63,23 @@ colors = z*1.0/rho.size+1.0/(rho.size*2)
 plt.scatter(x0s, x1s, c=colors)
 plt.show()
 
-##########################
-#Gibbs Sampler!
+# # From http://lmf-ramblings.blogspot.com/2009/07/multivariate-normal-distribution-in.html
+# # I could not find a Python function to evaluate the multivariate normal distribution in Python. Here's one that gives equivalent results to the dmvnorm function in the mvtnorm package for R. It's something that works. I've not had time or need yet to fix it up.
+
+# # b: A vector
+# # mean: The mean of the elements in b (same dimensions as b)
+# # cov: The covariance matrix of the multivariate normal distribution
+
+# # License: GPL version 2
+# # http://www.gnu.org/licenses/gpl-2.0.html
+# def dmvnorm(b,mean,cov):
+#     k = b.shape[0]
+#     part1 = np.exp(-0.5*k*np.log(2*np.pi))
+#     part2 = np.power(np.linalg.det(cov),-0.5)
+#     dev = b-mean
+#     part3 = np.exp(-0.5*np.dot(np.dot(dev.transpose(),np.linalg.inv(cov)),dev))
+#     dmvnorm = part1*part2*part3
+#     return dmvnorm
 
 #Adapted from https://github.com/danstowell/gmphd/blob/master/gmphd.py
 def dmvnorm(x,loc,cov):
@@ -73,16 +88,37 @@ def dmvnorm(x,loc,cov):
     #cov = np.array(cov, dtype=myfloat)
     #x = array(x, dtype=myfloat)
     k = len(loc)
-    pi = np.pi
     part1 = (2.0 * pi) ** (-k * 0.5)
     part2 = np.power(np.linalg.det(cov), -0.5)
     dev = x - loc
     part3 = np.exp(-0.5 * np.dot(np.dot(np.transpose(dev), np.linalg.inv(cov)), dev))
     return part1 * part2 * part3
 
+# Run a Gibbs sampler for a CRP Gaussian mixture model
+# on the data
+#
+# Args:
+#  data: an Ndata x D matrix of data points
+#  sd: we assume the Gaussian likelihood around any
+#      cluster mean has covariance matrix diag(sd^2,...,sd^2);
+#      so this is the standard deviation in any direction
+#  sd_prior: the prior is the covariance matrix diag(sd_prior^2,...,sd_prior^2);
+#  initz: vector of strictly positive integers; initial
+#      assignments of data points to clusters; takes
+#      values 1,...,K
+#  alpha: concentration parameter for CRP, small value
+#      steeres process toward small number of clusters
+#      reasonable default choice for demo: 0.01
+#
+# Returns:
+#  Nothing
+# Note: may only work for D=2
+# Note: I made the hardcoded parameters sd_prior and alpha
+#       explicit and added them to the function signature
+#       as parameters.
 def run_gibbs(data,sd,sd_prior,initz,alpha):
     # don't exceed this many Gibbs iterations
-    maxIters = 5
+    maxIters = 1000
 
     # the algorithm will pause and plot after this
     # iteration number; 0 ensures it will plot right off
@@ -91,7 +127,7 @@ def run_gibbs(data,sd,sd_prior,initz,alpha):
     # dimension of the data points (number of columns)
     data_dim = data.shape[1]
     # cluster-specific covariance matrix
-    Sig = np.diag(sd**2*np.ones(data_dim))
+    Sig = np.diag(sd^2*np.ones(data_dim))
     # prior covariance matrix
     Sig0 = np.diag(sd_prior**2*np.ones(data_dim))
     # cluster-specific precision (Sig^{-1})
@@ -101,39 +137,20 @@ def run_gibbs(data,sd,sd_prior,initz,alpha):
     # prior mean on cluster parameters
     mu0 = np.zeros(2)
     # number of data points
-    number_of_datapoints = data.shape[0]
-    
-    print 'initialization'
-    print alpha
-    print Sig
-    print Sig0
-    print Prec
-    print Prec0
-    print mu0
+    number_of_datapoints = data.shape[2]
 
     # initialize the sampler
     z = initz  # initial cluster assignments
     cluster_counts = []
     for cluster_label in np.unique(z):
-        vector_of_ones = [1 for z_elem in z if z_elem==cluster_label]
-        cluster_counts.append(sum(vector_of_ones))
+        cluster_counts.append(sum([1 for z_elem in z if z_elem==cluster_label]))
     number_of_clusters = len(cluster_counts)   # initial number of clusters
-    
+
     # run the Gibbs sampler
-    for iteration in range(maxIters):
-        
-        print
-        print 'iteration:',iteration
-        print
-        
+    for iteration in range(1,maxIters+1):
         # take a Gibbs step at each data point
-        for n in range(number_of_datapoints):
-            
-            print
-            print 'datapoint:', n
-            
+        for n in range(1,number_of_datapoints):
             # get rid of the nth data point
-            print 'cluster counts',cluster_counts
             cluster_id = z[n]
             cluster_counts[cluster_id] = cluster_counts[cluster_id] - 1
 
@@ -149,66 +166,31 @@ def run_gibbs(data,sd,sd_prior,initz,alpha):
                 cluster_counts = cluster_counts[:-1]
                 number_of_clusters = number_of_clusters - 1
             z[n] = -1  # ensures z[n] doesn't get counted as a cluster
-            print 'z for now',z
 
             # unnormalized log probabilities for the clusters
             log_weights = np.nan * np.ones(number_of_clusters+1)
             
             # find the unnormalized log probabilities
             # for each existing cluster
-            for cluster_id in range(number_of_clusters): 
-                print 'cluster_id', cluster_id
-                
-                c_Precision = Prec0 + cluster_counts[cluster_id] * Prec
+            for cluster_id in range(1,number_of_clusters+1): 
+                c_Precision = Prec0 + counts[cluster_id] * Prec
                 c_Sig = np.linalg.inv(c_Precision)
                 # find all of the points in this cluster
                 loc_z = z==cluster_id
                 # sum all the points in this cluster
-                data_slice_c = data[loc_z]
-                sum_data = data_slice_c.sum(axis=0)
-                
-                print 'sum_data', sum_data
-                
+                column_sums = data.sum(axis=1)
+                sum_data = column_sums[loc_z]
+
                 mat_prod1 = np.dot(Prec, sum_data)
                 mat_prod2 = np.dot(Prec0, np.transpose(mu0))
                 mat_sum = mat_prod1 + mat_prod2
                 c_mean = np.dot(c_Sig, mat_sum)
-                print 'c_mean',c_mean
                 
-                print 'cluster_counts[cluster_id]',cluster_counts[cluster_id]
-                print 'np.log(cluster_counts[cluster_id])',np.log(cluster_counts[cluster_id])
-                
-                dmvn = np.log(dmvnorm(data[n,:],c_mean,c_Sig + Sig))
-                print 'dmvn',dmvn
-                log_weights[cluster_id] = np.log(cluster_counts[cluster_id]) + dmvn
-                print 'log_weights updated',log_weights
-
+                log_weights[c] = np.log(cluster_counts[cluster_id]) + dmvnorm(data[n,:],c_mean,c_Sig + Sig)
+            }
             # find the unnormalized log probability
             # for the "new" cluster
-            dmvn = np.log(dmvnorm(data[n,:],mu0,Sig0 + Sig))
-            log_weights[number_of_clusters] = np.log(alpha) + dmvn
-            print 'new log_weights',log_weights
+            log_weights[number_of_clusters+1] = np.log(alpha) + dmvnorm(data[n,:],mu0,Sig0 + Sig)
 
-            # transform unnormalized log probabilities
-            # into probabilities
-            max_weight = np.max(log_weights)
-            log_weights = log_weights - max_weight
-            loc_probs = np.exp(log_weights)
-            loc_probs = loc_probs / np.sum(loc_probs)
-            
-            print 'loc_probs',loc_probs
 
-            # sample which cluster this point should belong to
-            newz = np.random.choice(range(number_of_clusters+1), replace=True, p=loc_probs)
-            print 'newz',newz
-            # if necessary, instantiate a new cluster
-            if newz == number_of_clusters:
-                cluster_counts.append(0)
-                number_of_clusters = number_of_clusters + 1
-            z[n] = newz
-            # update the cluster counts
-            cluster_counts[newz] = cluster_counts[newz] + 1
-        if iteration >= minPauseIter:
-            print z
-    print 'final z',z
-    return z
+
